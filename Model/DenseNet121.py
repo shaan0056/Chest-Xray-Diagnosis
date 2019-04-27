550a369b338723b020dcb65bde4484fb2c7971a6
Santhanu Venugopal Sunitha <ssunitha3@gatech.edu>
2019-04-27 09:29:51 -0400

    Updated Densenet

--- Model/DenseNet121.py
+++ Model/DenseNet121.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 
 class DenseNet121(nn.Module):
 
-    def __init__(self,return_size):
+    def __init__(self,return_size,freeze=False):
 
         super(DenseNet121,self).__init__()
         self.denseNet121 = torchvision.models.densenet121(pretrained=True)
-        for params in self.denseNet121.parameters():
-            params.requires_grad = False
+        if freeze:
+            for params in self.denseNet121.parameters():
+                params.requires_grad = False
         features = self.denseNet121.classifier.in_features
 
         self.denseNet121.classifier = nn.Sequential(
